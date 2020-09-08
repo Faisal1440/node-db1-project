1de26aaad199520f1cc579e8c76aca1a8a4f2297
Faisal Aziz <faziz6006@gmail.com>
2020-09-08 16:21:19 -0500

    endpt-2

--- data/seeds/accountRouter.js
+++ data/seeds/accountRouter.js
@@ -15,4 +15,30 @@ router.get("/", (req, res) => {
   })
 })
 
+router.get("/:id", (req, res) => {
+    db("accounts")
+    .where({ id: req.params.id})
+    .first()
+    .then(acc => {
+      res.status(200).json(acc)
+    })
+    .catch(err => {
+      console.log(err)
+        res.status(500).json({ error: "failed to get list of accounts" })
+    })
+  })  
+  
+  
+  router.post("/", (req, res) => {
+    const replace = req.body
+    db("accounts").insert(replace, "id")
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "failed to create new account" })
+  
+    })
+  })
 module.exports = router;
